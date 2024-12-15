115c77a46e29d5f0090a5e05ac6301709f29a992
jaythummar0408 <jppatel10125@gmail.com>
2024-12-15 15:53:24 +0530

    updated code with remove useefect which is not used

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import React  from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import AccountPage from './pages/AccountPage';
